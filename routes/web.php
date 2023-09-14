c3f58d90be1b95ebb582283b765314e6f08bc37f
Andres Link <andres.link@mail.ru>
2023-09-14 17:34:36 +0300

    create live chat with pusher

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Events\ChatMessage;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -43,3 +45,21 @@ Route::get('/search/{term}', [PostController::class, 'search']);
 Route::get('/admins-only', function() {
     return 'Welcome to admins page'; 
 })->middleware('can:visitAdminsPages');
+
+Route::post('/send-chat-message', function (Request $request) {
+    $formFields = $request->validate([
+        'textvalue' => 'required'
+    ]);
+
+    if (!trim(strip_tags($formFields['textvalue']))) {
+        return response()->noContent();
+    }
+
+    broadcast(new ChatMessage([
+        'username' => auth()->user()->username, 
+        'textvalue' => strip_tags($request->textvalue),
+        'avatar' => auth()->user()->avatar
+    ]))->toOthers();
+
+    return response()->noContent();
+})->middleware('mustBeLoggedIn');
